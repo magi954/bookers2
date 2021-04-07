8934b2fd652c2faab8bff1849e7867c4352722bf
utawareikka587@gmail.com <ec2-user@ip-172-31-32-21.ec2.internal>
2021-04-07 06:08:16 +0000

    [Add]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '038c2f0ce1702a039140c08e3e041900ae0ab1283e2caf520540e8356744ee7467608068f089e4794beeb9ad21272f63ed712c03377d16bdd2aea292da48a155'
